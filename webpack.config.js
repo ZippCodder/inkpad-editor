9962b124a67c5dda8ee67761d2b3441eb84e4bda
deonrich <deon.edward.rich@gmail.com>
2024-03-21 12:47:10 -0600

    adding final touches

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ const config = {
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                type: 'asset/inline',
             },
 
             // Add your rules for custom modules here
