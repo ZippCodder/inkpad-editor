16dde9ec86614df61787c196c21116d20bc2c77c
deonrich <deon.edward.rich@gmail.com>
2024-05-29 06:59:48 -0600

    separating dev and production mode for the server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require("http");
+const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
-const server = http.createServer();
+const protocol = (process.env.MODE === "development") ? http:https;
+const server = protocol.createServer((process.env.MODE === "production") ? {key: process.env.KEY_PATH, cert: process.env.CERT_PATH}:undefined);
 
 server.on("request",(req,res) => {
  let contentType = "text/plain", file;
@@ -50,7 +52,7 @@ server.on("request",(req,res) => {
  res.end(file);
 });
 
-server.listen(process.env.PORT || 80,function() {
+server.listen(process.env.PORT,function() {
  console.log("Server is active...");
 });
 
